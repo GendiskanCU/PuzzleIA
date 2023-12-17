ac7cfc3ddc23f77630c9321b413636ec637c07cb
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2023-12-17 21:32:40 +0100

    Inicio algoritmo resolver

--- Assets/Nodo.cs
+++ Assets/Nodo.cs
@@ -53,6 +53,18 @@ public class Nodo
     }
 
     /// <summary>
+    /// Copia el estado del nodo original
+    /// </summary>
+    /// <param name="nodoOriginal"></param>
+    public void CopiarEstado(Nodo nodoOriginal)
+    {
+        for(int i = 0; i < 9; i++)
+            {
+                piezas[i] = nodoOriginal.piezas[i];
+            }
+    }
+
+    /// <summary>
     /// Devuelve la posición del hueco en blanco en el nodo
     /// </summary>
     /// <returns></returns>
@@ -71,9 +83,74 @@ public class Nodo
     /// <returns>Nodos sucesores</returns>
     public List<Nodo> ObtenerSucesores()
     {
+        //Lista de sucesores a obtener y devolver
         List<Nodo> sucesores = new List<Nodo>();
 
+        //Listas nombradas con la dirección a la que se puede mover el hueco
+        //si actualmente está en alguna de las posiciones de cada lista
+        List<int> HaciaDerecha = new List<int> {0, 1, 3, 4, 6, 7};
+        List<int> HaciaIzquierda = new List<int> {1, 2, 4, 5, 7, 8};
+        List<int> HaciaArriba = new List<int> {3, 4, 5, 6, 7, 8};
+        List<int> HaciaAbajo = new List<int> {0, 1, 2, 3, 4, 5};        
+        
+
+        //Obtiene las posibles nuevas posiciones del hueco y nodos resultantes.
+        //Si el hueco actualmente está en la posición de alguna de las listas
+        //se calcula el nodo resultante del movimiento a la dirección adecuada
+        //y se añade a la lista de nodos sucesores
+        int posicionActual = PosicionHueco();
+        int nuevaPosicionHueco;
+
+        if(HaciaDerecha.Contains(posicionActual))
+        {
+            nuevaPosicionHueco = posicionActual + 1;
+            sucesores.Add(Sucesor(posicionActual, nuevaPosicionHueco));
+            
+            Debug.Log("\nNodo sucesor añadido:\n" + Sucesor(posicionActual, nuevaPosicionHueco).Dibujar());
+        }
+
+        if(HaciaIzquierda.Contains(posicionActual))
+        {
+            nuevaPosicionHueco = posicionActual - 1;
+            sucesores.Add(Sucesor(posicionActual, nuevaPosicionHueco));
+            
+            Debug.Log("\nNodo sucesor añadido:\n" + Sucesor(posicionActual, nuevaPosicionHueco).Dibujar());
+        }
+
+        if(HaciaArriba.Contains(posicionActual))
+        {
+            nuevaPosicionHueco = posicionActual - 3;
+            sucesores.Add(Sucesor(posicionActual, nuevaPosicionHueco));
+            
+            Debug.Log("\nNodo sucesor añadido:\n" + Sucesor(posicionActual, nuevaPosicionHueco).Dibujar());
+        }
+
+        if(HaciaAbajo.Contains(posicionActual))
+        {
+            nuevaPosicionHueco = posicionActual + 3;
+            sucesores.Add(Sucesor(posicionActual, nuevaPosicionHueco));
+            
+            Debug.Log("\nNodo sucesor añadido:\n" + Sucesor(posicionActual, nuevaPosicionHueco).Dibujar());
+        }
+
+        
         return sucesores;
+    }
+
+    private Nodo Sucesor(int posActual, int posNueva)
+    {
+        //Crea un nuevo nodo temporal para trabajar con él sin modificar el original
+        Nodo nodoTemporal = new Nodo();
+        //Copia el estado del nodo original al temporal
+        nodoTemporal.CopiarEstado(this);
+
+        int temporal = nodoTemporal.piezas[posNueva];
+        //Coloca el hueco en su nueva posición
+        nodoTemporal.piezas[posNueva] = 0;
+        //Se intercambia por el valor que había en ella
+        nodoTemporal.piezas[posActual] = temporal;
+
+        return nodoTemporal;       
     }
 
     
--- Assets/PuzzleIA.cs
+++ Assets/PuzzleIA.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PuzzleIA : MonoBehaviour
@@ -13,6 +14,15 @@ public class PuzzleIA : MonoBehaviour
     //El meta es un Nodo recién creado, sin desordenar
     private Nodo nodoMeta = new Nodo();
 
+    //Lista de nodos sucesores del actual
+    private List<Nodo> nodosSucesores = new List<Nodo>();
+
+    //Lista de nodos abiertos, pendientes de comprobar;
+    private List<Nodo> nodosAbiertos = new List<Nodo>();
+
+    //Lista de nodos cerrados, ya comprobados
+    private List<Nodo> nodosCerrados = new List<Nodo>();
+
     // Start is called before the first frame update
     void Start()
     {        
@@ -22,12 +32,84 @@ public class PuzzleIA : MonoBehaviour
    
    public void Desordenar()
    {
-        DesordenaNodo();
+        //Desordena evitando que por azar resulte el nodo ya ordenado
+        bool continua = true;
+        while(continua)
+        {
+            DesordenaNodo();
+            if (!nodoActual.EsIgualA(nodoMeta))
+                continua = false;
+        }
+        //Tras desordenar hay que reiniciar todas las listas        
+        nodosSucesores.Clear();
+        nodosAbiertos.Clear();
+        nodosCerrados.Clear();
    }
 
     public void Resolver()
     {        
+        nodosSucesores = nodoActual.ObtenerSucesores();
         
+        /*Para comprobación interna desde la consola*******/
+        /**/Debug.Log("Lista de nodos sucesores:\n");   /**/
+        /**/foreach(Nodo n in nodosSucesores)           /**/        
+        /**/    Debug.Log(n.Dibujar());                 /**/        
+        /**************************************************/
+
+        //Incluye los primeros sucesores en la lista de abiertos
+        foreach(Nodo n in nodosSucesores)
+            nodosAbiertos.Add(n);
+        //El primer nodo actual se añade a la lista de cerrados
+        nodosCerrados.Add(nodoActual);
+        
+        int numeroIntentos = 0;
+
+        while(nodosAbiertos.Count > 0)
+        {
+            numeroIntentos++;
+            //El primer nodo abierto se convierte en el actual
+            nodoActual.CopiarEstado(nodosAbiertos[0]);
+            //Lo sacamos de abiertos
+            nodosAbiertos.Remove(nodosAbiertos[0]);
+            //Añadimos el nodo actual a cerrados
+            nodosCerrados.Add(nodoActual);
+
+            //Si el actual es el meta
+            if(nodoActual.EsIgualA(nodoMeta))
+            {
+                Debug.Log("Obtenido el resultado en " + numeroIntentos + " intentos");
+                cajaDibujo.text = nodoActual.Dibujar();
+                break;
+            }
+            else
+            {
+                //Obtiene los sucesores del nodo actual
+                nodosSucesores = nodoActual.ObtenerSucesores();
+                //Añadimos a abiertos los sucesores que no estén ya en abiertos o cerrados
+                //Primero obtenemos la posición de los sucesores a añadir
+                List<int> posiciones = ObtieneIndicesNodosNuevos();
+                if(posiciones.Count > 0)
+                {
+                    foreach(int posicion in posiciones)
+                    {
+                        nodosAbiertos.Add(nodosSucesores[posicion]);
+                    }
+                }                
+            }
+        }
+    }
+
+    private List<int> ObtieneIndicesNodosNuevos()
+    {
+        List<int> indices = new List<int>();
+
+       for(int s = 0; s < nodosSucesores.Count; s++)
+       {
+            Debug.Log(nodosSucesores.Count + "  " + nodosAbiertos.Count);
+            Debug.Log(nodosSucesores[s].EsIgualA(nodosAbiertos[0]));
+       }
+
+        return indices;
     }
 
 
